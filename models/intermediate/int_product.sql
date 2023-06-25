955cd2ff6b3fa78fd31922829d3534ec9049fbbc
Thaina <thaina.moraes.contractor@pepsico.com>
2023-06-25 10:23:13 -0300

    modifying files stg

--- models/intermediate/int_product.sql
+++ models/intermediate/int_product.sql
@@ -1,6 +1,6 @@
 with product as (
     select * 
-    from {{ ref('stg_product') }}
+    from {{ ref('stg_product') }} 
 )
 
 , subcategory as (
@@ -13,9 +13,9 @@ with product as (
     from {{ ref('stg_category') }}
 )
 
-, description as (
+, inventory as (
     select * 
-    from {{ ref('stg_description') }}
+    from {{ ref('stg_inventory') }}
 )
 
 , location as (
@@ -28,34 +28,46 @@ with product as (
     from {{ ref('stg_model') }}
 )
 
-, inventory as (
-    select * 
-)
-
 , union_all as (
     select 
-    product_id
-    , product_model_id
-    , product_subcategory_id
-    , product_name
-    , product_number
-    , makeflag
-    , finished_goods_flag
-    , color
-    , safety_stock_level
-    , stand_cost
-    , list_price
-    , size_product
-    , style_product
-    , weight_product 
+        product.product_id
+        , product_model_id
+        , subcategory_id
+        , category.category_id
+        , location.location_id
+        , category_name
+        , subcategory_name
+        , product_name
+        , product_number
+        , makeflag
+        , finished_goods_flag
+        , color
+        , safety_stock_level
+        , stand_cost
+        , list_price
+        , size_product
+        , style_product
+        , weight_product
+        , location_name
+        , cost_rate
+        , availability
+        , shelf
+        , bin
+        , quantity_inventory
+        , model_name
     from product
     left join subcategory
     on product.product_subcategory_id = subcategory.subcategory_id
     left join category
     on subcategory.category_id = category.category_id
+    left join inventory 
+    on inventory.product_id = product.product_id
+  	left join location
+  	on location.location_id = inventory.location_id
     left join model
     on product.product_model_id = model.model_id
 )
 
 select * 
 from union_all
+
--- models/silver/sales/stg_sales_order_detail.sql
+++ models/silver/sales/stg_sales_order_detail.sql
@@ -1,10 +1,18 @@
 with source_data as (
   select 
-    customerid as customer_id
-    personid as person_id
-    storeid as store_id
-    territoryid as territoryid
-  from  {{source('source_dw','customer')}}
+    salesorderid as salesorder_id
+    , salesorderdetail as sales_order_detail
+    -- Shipment tracking number supplied by the shipper.
+    , carriertrackingnumber as carrier_tracking_number
+    , orderqty as order_qty
+    , productid as product_id
+    -- Promotional code. Foreign key to SpecialOffer.SpecialOfferID.
+    , specialofferid as special_offer_id
+    , unitprice as unit_price
+    , unitpricediscount as unit_price_discount
+    /* Per product subtotal. Computed as UnitPrice * (1 - UnitPriceDiscount) * OrderQty.
+      Computed: isnull(([UnitPrice]*((1.0)-[UnitPriceDiscount]))*[OrderQty],(0.0))*/ 
+  from  {{source('source_dw','salesorderdetail')}}
 )
 
 select *
