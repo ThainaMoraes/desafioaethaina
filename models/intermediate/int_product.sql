b34105cca295aee65fdb835b0bddedbbfdf98701
Thaina <thaina.moraes.contractor@pepsico.com>
2023-07-06 13:28:13 -0300

    final fix for BI

--- models/intermediate/int_product.sql
+++ models/intermediate/int_product.sql
@@ -25,7 +25,28 @@ with product as (
 
 , union_product_subcategory as (
     select 
-       product.*
+        product_id
+        , product_model_id
+        , product_subcategory_id
+        , product_name
+        , product_number
+        , makeflag
+        , finished_goods_flag
+        , color
+        , safety_stock_level
+        , stand_cost
+        , list_price
+        , size_product
+        , weight_product 
+        , case 
+            when style_product = 'W'
+                then 'Feminino'
+            when style_product = 'M'
+                then 'Masculino'
+            when style_product = 'U'
+                then 'Unisex'
+            else 'Não definido'
+        end as style_product
        , category_id
        , subcategory_name
     from product
